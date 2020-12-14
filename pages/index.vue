bd12d0814adf881c3b34b0a1f246d6e12a7e826b
ichiki-hiromsa <70831210+ichiki-hiromsa@users.noreply.github.com>
2020-12-14 20:12:15 +0900

    first

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container>
+    <div>
+      <v-text-field v-model="mail"></v-text-field>
+      <v-text-field v-model="password"></v-text-field>
+      <v-btn @click="createNewAccount">
+        新規作成
+      </v-btn>
+    </div>
+  </v-container>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  },
+  data:()=>({
+    mail:"",
+    password:""
+  }),
+  methods:{
+    createNewAccount(){
+      const auth=this.$firebase.auth()
+      auth.createUserWithEmailAndPassword(this.mail,this.password)
+      .then(user=>{
+        console.log(user)
+      })
+    }
+  }
+}
+</script>
---
+++ plugins/firebase.js
@@ -0,0 +1,26 @@
+// import * as firebase from 'firebase/app'
+import firebase from 'firebase';
+import 'firebase/firestore';
+
+//20201209 認証
+import 'firebase/auth';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDPmTuxn4hqevx0vlAeZx2SKnUjsEl8l-A",
+  authDomain: "trade-app-ichi.firebaseapp.com",
+  databaseURL: "https://trade-app-ichi.firebaseio.com",
+  projectId: "trade-app-ichi",
+  storageBucket: "trade-app-ichi.appspot.com",
+  messagingSenderId: "252217173678",
+  appId: "1:252217173678:web:2e02c94eaa334ad4686f3f"
+};
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+// nuxtの書き方、どこでも使えるように
+export default function(app,inject){
+  inject('firebase',firebase)
+}
